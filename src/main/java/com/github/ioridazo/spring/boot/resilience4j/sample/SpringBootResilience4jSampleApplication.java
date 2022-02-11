c752f37866520e7bebe5343af5434bf8518e9320
iori-oiso <iori.dazo@gmail.com>
2022-02-11 16:58:25 +0900

    init

---
+++ src/main/java/com/github/ioridazo/spring/boot/resilience4j/sample/SpringBootResilience4jSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.ioridazo.spring.boot.resilience4j.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResilience4jSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootResilience4jSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ioridazo/spring/boot/resilience4j/sample/SpringBootResilience4jSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ioridazo.spring.boot.resilience4j.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResilience4jSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
